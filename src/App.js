be3397b2949f5c4bbd714f201ae8424cf2a6bd2b
dor balmas <dorbal31@gmail.com>
2020-08-02 14:35:32 +0300

    30 7 2020

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-// import AppDays from './daysCalc/appDays';
-import AppSuper from './super_comps_hooks/appSuper';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+// import AppDays from "./daysCalc/appDays";
+import AppSuper from "./super_comps_hooks/appSuper";
 // import AppDate from './date_comps/appDate';
-// import AppHooks from './hooks_comps/app_hooks';
+// import AppHooks from "./hooks_comps/app_hooks";
 
 function App() {
   return (
