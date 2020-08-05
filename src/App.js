92b09cc3fde7e70a687d9a2c63f632029419408a
dor balmas <dorbal31@gmail.com>
2020-08-05 13:18:42 +0300

    30 7 2020

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-    return ( <
-        div className = "App" >
-        start <
-        /div>
-    );
+  return <div className="App">start </div>;
 }
 
 export default App;
