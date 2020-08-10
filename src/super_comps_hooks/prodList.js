09725fcc2b2fef1cdbd51e0035fca2c559d30c91
dor balmas <dorbal31@gmail.com>
2020-08-10 20:06:56 +0300

    10082020

--- src/super_comps_hooks/prodList.js
+++ src/super_comps_hooks/prodList.js
@@ -1,39 +1,36 @@
-import React,{useState, useEffect} from 'react';
-import ProdItem from './prodItem';
-import { doApiGet } from '../services/apiService';
-import "./css/super.css"
+import React, { useState, useEffect } from "react";
+import ProdItem from "./prodItem";
+import { doApiGet } from "../services/apiService";
+import "./css/super.css";
 
-function ProdList(props){
-  let [prods_ar,setProds] = useState([])
-
+function ProdList(props) {
+  let [prods_ar, setProds] = useState([]);
 
   //component did mount
   useEffect(() => {
-    let url = "http://localhost:3000/prods"
-    if(props.match){
-      if(props.match.params.catId){
-        url = "http://localhost:3000/prods/cat/"+props.match.params.catId
+    let url = "http://localhost:3000/prods";
+    if (props.match) {
+      if (props.match.params.catId) {
+        url = "http://localhost:3000/prods/cat/" + props.match.params.catId;
       }
 
-      if(props.match.params.qsearch){
-        url = "http://localhost:3000/prods/search/?q="+props.match.params.qsearch
+      if (props.match.params.qsearch) {
+        url =
+          "http://localhost:3000/prods/search/?q=" + props.match.params.qsearch;
       }
     }
-    doApiGet(url)
-    .then(data => {
-      setProds(data)
-    })
-  },[props.match])
+    doApiGet(url).then((data) => {
+      setProds(data);
+    });
+  }, [props.match]);
 
-  return(
+  return (
     <div className="row">
-      {prods_ar.map(item => {
-        return (
-          <ProdItem key={item._id} item={item}/>
-        )
+      {prods_ar.map((item) => {
+        return <ProdItem key={item._id} item={item} />;
       })}
-    </div> 
-  )
+    </div>
+  );
 }
 
-export default ProdList
+export default ProdList;
